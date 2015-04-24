b19ada3773b4c2b7402443395bcbd4565e9c7010
Austin Erlandson <austin@erlandson.com>
2015-04-24 11:56:26 -0500

    Refactored month.to_s and wrote unit tests for Day

--- lib/month.rb
+++ lib/month.rb
@@ -13,15 +13,7 @@ class Month < Day
 
   def to_s
     months = %w[January February March April May June July August September October November December]
-    month_str = months[month - 1]
-    year_str_length = 5
-    center_col = 10
-    header_diff_str = ''
-    header_diff = center_col - ((month_str.length + year_str_length) / 2)
-    header_diff.times do
-      header_diff_str << ' '
-    end
-    month = header_diff_str + month_str
+    header = months[month - 1] + " #{year}"
     days_of_month = <<EOS
  1  2  3  4  5  6  7
  8  9 10 11 12 13 14
@@ -30,7 +22,7 @@ class Month < Day
 29 30 31
 EOS
     str = <<EOS
-#{month} #{year}
+#{header.center(20).rstrip}
 Su Mo Tu We Th Fr Sa
 #{days_of_month}
 EOS
--- test/test_day.rb
+++ test/test_day.rb
@@ -12,16 +12,45 @@ class TestDay < Minitest::Test
 
   def test_zellers_returns_zero
     d = Day.new(1, 1, 2012)
-    z = d.zellers(1, 13, 99, 19)
+    z = d.zellers(1, 13, 12, 20)
     assert_equal 0, z
   end
 
+  def test_zellers_returns_one
+    d = Day.new(1, 1, 2012)
+    z = d.zellers(1, 5, 49, 22)
+    assert_equal 1, z
+  end
 
+  def test_zellers_returns_three
+    d = Day.new(1, 1, 2012)
+    z = d.zellers(2, 6, 21, 20)
+    assert_equal 1, z
+  end
 
+  def test_zellers_returns_four
+    d = Day.new(1, 1, 2012)
+    z = d.zellers(2, 6, 21, 20)
+    assert_equal 1, z
+  end
 
+  def test_zellers_returns_five
+    d = Day.new(1, 1, 2012)
+    z = d.zellers(2, 6, 21, 20)
+    assert_equal 1, z
+  end
 
+  def test_zellers_returns_six
+    d = Day.new(1, 1, 2012)
+    z = d.zellers(2, 6, 21, 20)
+    assert_equal 1, z
+  end
 
-
+  def test_zellers_returns_seven
+    d = Day.new(1, 1, 2012)
+    z = d.zellers(2, 6, 21, 20)
+    assert_equal 1, z
+  end
 
 
 
