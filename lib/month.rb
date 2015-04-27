126afb3912b727c51b6264268b453f1815c1aae8
Austin Erlandson <austin@erlandson.com>
2015-04-27 12:07:42 -0500

    Refactored month.to_s but still needs work:)

--- lib/month.rb
+++ lib/month.rb
@@ -1,53 +1,34 @@
 require_relative '../lib/day'
 
 class Month < Day
-  attr_reader :month, :year
+  attr_reader :month, :year, :months, :month_lengths, :first_day_of_month
 
   def initialize(month, year)
     @month = month
     @year = year
-  end
-
-  def to_s
-    months = %w[January February March April May June July August September October November December]
-    header = months[month - 1] + " #{year}"
-
-    month_lengths = {
+    @months = %w[January February March April May June July August September October November December]
+    @month_lengths = {
        1 => 31, 2 => year % 4 == 0 ? 29 : 28, 3 => 31, 4 => 30,
        5 => 31, 6 => 30, 7 => 31, 8 => 31, 9 => 30, 10 => 31,
       11 => 30, 12 => 31
     }
+    @first_day_of_month = Day.new(1, month, year).zellers(1, month, year)
+  end
 
-    month_length = month_lengths[month]
-
+  def to_s
+    header = months[month - 1] + " #{year}"
     str = <<-eos
 #{header.center(20).rstrip}
 Su Mo Tu We Th Fr Sa
 eos
 
     days_of_month = ""
-    day = Day.new(1, month, year)
-    first_day_of_month = day.zellers(1, month, year)
-
-    if first_day_of_month == 1
-      days_of_month = days_of_month.rjust(3)
-    elsif first_day_of_month == 2
-      days_of_month =days_of_month.rjust(6)
-    elsif first_day_of_month == 3
-      days_of_month = days_of_month.rjust(9)
-    elsif first_day_of_month == 4
-      days_of_month = days_of_month.rjust(12)
-    elsif first_day_of_month == 5
-      days_of_month = days_of_month.rjust(15)
-    elsif first_day_of_month == 6
-      days_of_month = days_of_month.rjust(18)
-    end
-
+    days_of_month = days_of_month.rjust(first_day_of_month * 3)
     week_length = 0
 
-    month_length.times do |i|
+    month_lengths[month].times do |i|
       if days_of_month.length > 20
-        str << days_of_month + "\n"
+        str << days_of_month.rstrip + "\n"
         days_of_month = ""
         week_length += 1
       end
@@ -55,11 +36,11 @@ eos
       days_of_month << "#{i + 1} "
     end
     if week_length == 4
-      str << days_of_month + "\n\n"
+      str << days_of_month.rstrip + "\n\n"
     else
-      str << days_of_month + "\n"
+      str << days_of_month.rstrip + "\n"
     end
-    return str.gsub(/\s\n/, "\n")
+    return str
   end
 end
 
