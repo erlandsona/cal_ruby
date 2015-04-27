3e177543b9dd8674130ee62f1a5c8ba372e4f220
Austin Erlandson <austin@erlandson.com>
2015-04-27 10:46:57 -0500

    Month.to_s passing!!! Now to refactor the Behemoth

--- lib/month.rb
+++ lib/month.rb
@@ -8,26 +8,6 @@ class Month < Day
     @year = year
   end
 
-  # This to_s method is based on a fixed width font and puts out a 20 column wide month. All "Magic numbers are based on the width of the string:)"
-
-  #def to_s
-    #months = %w[January February March April May June July August September October November December]
-    #header = months[month - 1] + " #{year}"
-    #days_of_month = <<EOS
- #1  2  3  4  5  6  7
- #8  9 10 11 12 13 14
-#15 16 17 18 19 20 21
-#22 23 24 25 26 27 28
-#29 30 31
-#EOS
-    #str = <<EOS
-##{header.center(20).rstrip}
-#Su Mo Tu We Th Fr Sa
-##{days_of_month}
-#EOS
-  #end
-
-
   def to_s
     months = %w[January February March April May June July August September October November December]
     header = months[month - 1] + " #{year}"
@@ -48,7 +28,7 @@ class Month < Day
     }
 
     month_length = month_lengths[month]
-    days_of_month = ''
+    days_of_month = ""
 
     day = Day.new(1, month, year)
     first_day_of_month = day.zellers(1, month, year)
@@ -57,18 +37,36 @@ class Month < Day
 Su Mo Tu We Th Fr Sa
 eos
 
-    #first_day_of_month.times do |i|
-      #days_of_month << "  " 
-    #end
+    if first_day_of_month == 0
+      days_of_month = days_of_month.rjust(18)
+    elsif first_day_of_month == 2
+      days_of_month =days_of_month.rjust(3)
+    elsif first_day_of_month == 3
+      days_of_month = days_of_month.rjust(6)
+    elsif first_day_of_month == 4
+      days_of_month = days_of_month.rjust(9)
+    elsif first_day_of_month == 5
+      days_of_month = days_of_month.rjust(12)
+    elsif first_day_of_month == 6
+      days_of_month = days_of_month.rjust(15)
+    end
+
+    week_length = 0
+
     month_length.times do |i|
       if days_of_month.length > 20
         str << days_of_month + "\n"
-        days_of_month = ''
+        days_of_month = ""
+        week_length += 1
       end
       i < 9 ? days_of_month << " #{i + 1} " :
       days_of_month << "#{i + 1} "
     end
-    str << days_of_month + "\n\n"
+    if week_length == 4
+      str << days_of_month + "\n\n"
+    else
+      str << days_of_month + "\n"
+    end
     return str.gsub(/\s\n/, "\n")
   end
 end
--- test/test_month.rb
+++ test/test_month.rb
@@ -39,7 +39,7 @@ EOS
     assert_equal expected, m.to_s
   end
 
-  def test_to_s_on_march_2015
+  def test_to_s_on_may_2016
     m = Month.new(05, 2016)
     expected = <<EOS
       May 2016
@@ -53,5 +53,97 @@ Su Mo Tu We Th Fr Sa
 EOS
     assert_equal expected, m.to_s
   end
+
+  def test_to_s_on_jan_2000
+    m = Month.new(1, 2000)
+    expected = <<EOS
+    January 2000
+Su Mo Tu We Th Fr Sa
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_on_may_2000
+    m = Month.new(5, 2000)
+    expected = <<EOS
+      May 2000
+Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30 31
+
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_on_feb_2000
+    m = Month.new(2, 2000)
+    expected = <<EOS
+   February 2000
+Su Mo Tu We Th Fr Sa
+       1  2  3  4  5
+ 6  7  8  9 10 11 12
+13 14 15 16 17 18 19
+20 21 22 23 24 25 26
+27 28 29
+
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_on_march_2000
+    m = Month.new(3, 2000)
+    expected = <<EOS
+     March 2000
+Su Mo Tu We Th Fr Sa
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29 30 31
+
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_on_jun_2000
+    m = Month.new(6, 2000)
+    expected = <<EOS
+     June 2000
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 19 20 21 22 23 24
+25 26 27 28 29 30
+
+EOS
+    assert_equal expected, m.to_s
+  end
+
+  def test_to_s_on_sep_2000
+    m = Month.new(9, 2000)
+    expected = <<EOS
+   September 2000
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+
+EOS
+    assert_equal expected, m.to_s
+  end
+
+
 
 end
